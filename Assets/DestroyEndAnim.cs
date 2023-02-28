150295fb4413abe8494eb3eb55860773b9a73a88
Félix Tellitocci <73948501+Magiccarpet00@users.noreply.github.com>
2023-02-28 16:34:32 +0100

    over spot
    pas terible mais il fera l'affaire pour le moment

---
+++ Assets/DestroyEndAnim.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyEndAnim : MonoBehaviour
+{
+    public void DestroyEnd()
+    {
+        Destroy(this.gameObject);
+    }
+}
--- Assets/Script/Spot.cs
+++ Assets/Script/Spot.cs
@@ -6,7 +6,10 @@ public class Spot : MonoBehaviour
 {
     [SerializeField] private List<GameObject> adjacentSpots = new List<GameObject>();
     [SerializeField] private List<GameObject> adjacentSecretSpots = new List<GameObject>();
-
+    
+    //FX
+    [SerializeField] private GameObject prefabOverMouse_fx;
+    private Animator AnimatorOverMouse_fx;
 
     public void AddAdjacentSpots(GameObject newSpot, bool secretSpot)
     {
@@ -21,9 +24,17 @@ public class Spot : MonoBehaviour
         return adjacentSpots;
     }
 
+
+
+    //FX
     void OnMouseEnter()
     {
-        //TODO FX
-        Debug.Log("Mouse is over GameObject.");
+        GameObject over = Instantiate(prefabOverMouse_fx, this.transform.position, Quaternion.identity);
+        AnimatorOverMouse_fx = over.GetComponent<Animator>();
+    }
+
+    void OnMouseExit()
+    {
+        AnimatorOverMouse_fx.SetTrigger("end");
     }
 }
